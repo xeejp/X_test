119ce099b61b09466af04ef36d47be884513c496
ryo33 <ryo12redstone@gmail.com>
2016-01-08 19:30:31 +0900

    Modify to write tests

--- script.py
+++ script.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
 import json
 import sys
 
+def dumps(data):
+    return json.dumps(data, sort_keys=True)
+
 if __name__ == "__main__":
     if len(sys.argv) == 1 + 1 and sys.argv[1] == "init":
-        print(json.dumps({'data': [], 'host': [], 'participant': {}}))
+        print(dumps({'data': [], 'host': [], 'participant': {}}))
     elif len(sys.argv) == 3 + 1 and sys.argv[1] == "join":
         old = json.loads(sys.argv[2])
         ID = sys.argv[3]
         old["data"].append(ID)
         old["participant"][ID] = []
-        print(json.dumps(old))
+        print(dumps(old))
     elif sys.argv[1] == "receive":
         old = json.loads(sys.argv[2])
         recieved = json.loads(sys.argv[3])
@@ -19,5 +23,4 @@ if __name__ == "__main__":
         elif len(sys.argv) == 4 + 1:
             ID = sys.argv[4]
             old["participant"][ID].append(recieved)
-        print(json.dumps(old))
-
+        print(dumps(old))
