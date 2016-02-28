c54d4fd0919bb9b835e36e3a889eb189ba571760
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-02-28 20:16:06 +0900

    Update api

--- host.js
+++ host.js
@@ -2,3 +2,8 @@
 function onUpdate(data) {
     console.log(data)
 }
+var _ex = require("web/static/js/app")
+var _experiment = new _ex.Experiment(_topic, _token, onUpdate)
+function send_data(data) {
+  _experiment.send_data(data)
+}
